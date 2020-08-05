679333999efe26564e382b19da6508f2ddd8e78e
Ivan Diaz <ild2005@med.cornell.edu>
2020-08-04 23:35:09 -0400

    Update print.R

--- R/print.R
+++ R/print.R
@@ -1,5 +1,20 @@
 print.adjusted_rmst <- function(x){
-  cat('Estimate for control arm (A == 0): ', x$rmst['rmst0'], '\n')
-  cat('Estimate for treatment arm (A == 1): ', x$rmst['rmst1'], '\n') 
+  cat('RMST for control arm (A == 0): ', x$rmst['rmst0'], '\n')
+  cat('RMST for treatment arm (A == 1): ', x$rmst['rmst1'], '\n') 
   cat('Standard error for the difference: ', x$std.error.diff, '\n') 
+}
+print.ipw_rmst <- function(x){
+  cat('RMST for control arm (A == 0): ', x$rmst['rmst0'], '\n')
+  cat('RMST for treatment arm (A == 1): ', x$rmst['rmst1'], '\n') 
+  cat('Standard errors not available for IPW\n') 
+}
+print.adjusted_prob <- function(x){
+  cat('Survival probability for control arm (A == 0): ', x$prob['prob0'], '\n')
+  cat('Survival probability for treatment arm (A == 1): ', x$prob['prob1'], '\n') 
+  cat('Standard error for the difference: ', x$std.error.diff, '\n') 
+}
+print.ipw_prob <- function(x){
+  cat('Survival probability for control arm (A == 0): ', x$prob['prob0'], '\n')
+  cat('Survival probability for treatment arm (A == 1): ', x$prob['prob1'], '\n') 
+  cat('Standard errors not available for IPW\n') 
 }
